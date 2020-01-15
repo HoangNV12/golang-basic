57da358dc3162da01384d99a2443ec2c52fde0e6
HoangNV12 <tuphuchuy0304@gmail.com>
2020-01-15 16:29:36 +0700

    channel example

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+var wg sync.WaitGroup
 
+func printCounts(label string, count chan int) {
+	defer wg.Done()
+	for {
+		val, ok := <- count
+		if !ok {
+			fmt.Println("Channel was closed")
+			return
+		}
+		fmt.Printf("Count: %d received from %s \n", val, label)
+		if val == 10 {
+			fmt.Printf("Channel closed from %s \n", label)
+			close(count)
+			return
+		}
+		val++
+		count <- val
+	}
+}
 func main() {
-	messages := make(chan string, 2)
-
-	messages <- "Golang"
-
-	value := <- messages
-
-	fmt.Println(value)
+	count := make(chan int)
+	wg.Add(2)
+	fmt.Println("Start Goroutines")
+	go printCounts("A", count)
+	go printCounts("B", count)
+	count <- 1
+	fmt.Println("Waiting to finish")
+	wg.Wait()
+	fmt.Println("\nTerminating program")
 }
